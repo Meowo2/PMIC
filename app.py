ed8abab86ca46b31497f6aaa978ca00b0eb1f907
Ryan Araujo <arthursousasilva8@gmail.com>
2023-05-12 08:38:59 -0300

    Banco de Dados integrado

--- app.py
+++ app.py
@@ -1,13 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, jsonify
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+
+#Localiza o Banco de dados
+
+app.config['MYSQL_Host'] = 'localhost' # 127.0.0.1
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'fatec'
+app.config['MYSQL_DB'] = 'contatos'
+
+mysql = MySQL(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/contatos')
+@app.route('/contatos', methods=['GET', 'POST'])
 def contatos():
+    if request.method == 'POST':
+
+        email = request.form['email']
+        assunto = request.form['assunto']
+        descricao = request.form['descricao']
+
+        cur = mysql.connection.cursor() #Conecta Com a Base de Dados
+        cur.execute('INSERT INTO contatos (email,assunto,descricao) VALUES (%s, %s, %s)', (email, assunto, descricao)) #Executa Linha no "MySQL Workbench"
+
+        mysql.connection.commit()
+
+        cur.close() #Desconecta da Base de Dados
+        return 'Concluido'
     return render_template('contatos.html')
 
 @app.route('/quemsomos')
